bfb9bf0b730b7e732b48c3c5fd5e1cce2d014150
MichaelisTrofficus <miguel.otero.pedrido.1993@gmail.com>
2023-03-03 13:43:01 +0100

    Complete tests and add GitHub Actions

--- noxfile.py
+++ noxfile.py
@@ -1,8 +1,7 @@
 import nox
 
 
-@nox.session(python="3.9")
+@nox.session(python=["3.8", "3.9", "3.10", "3.11"])
 def tests(session):
     session.install("-r", "requirements_dev.txt")
     session.run('pytest')
-    session.notify("coverage")
--- pure_fasttext/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-FastText prediction class in pure python
-"""
-
-from .FastText import _FastText as FastText
-
-__all__ = ["FastText"]
